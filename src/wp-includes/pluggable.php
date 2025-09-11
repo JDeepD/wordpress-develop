415364d94ae5264a7aaf438a4a45cb0d56ecd069
Jaydeep Das <jaydeep.das@rtcamp.com>
2025-09-11 16:05:51 +0530

    upd: add testcases for encoded headers

--- src/wp-includes/pluggable.php
+++ src/wp-includes/pluggable.php
@@ -605,9 +605,9 @@ if ( ! function_exists( 'wp_mail' ) ) :
 		 */
 		do_action_ref_array( 'phpmailer_init', array( &$phpmailer ) );
 
-		$mail_data = array(
-			'to' => $raw_to,
-			...compact( 'subject', 'message', 'headers', 'attachments', 'embeds' ),
+		$mail_data = array_merge(
+			array( 'to' => $raw_to ),
+			compact( 'subject', 'message', 'headers', 'attachments' )
 		);
 
 		// Send!
